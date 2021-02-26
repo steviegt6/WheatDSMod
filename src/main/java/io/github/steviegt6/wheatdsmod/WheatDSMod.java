381afc875906df6561b3e9208095960f5af00f27
Stevie <xxlennygamerxx@gmail.com>
2021-02-25 22:18:36 -0800

    Rename files

--- src/main/java/io/github/steviegt6/wheatdsmod/WheatDSMod.java
+++ src/main/java/io/github/steviegt6/wheatdsmod/WheatDSMod.java
@@ -2,7 +2,7 @@ package io.github.steviegt6.wheatdsmod;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Wheatdsmod implements ModInitializer {
+public class WheatDSMod implements ModInitializer {
     @Override
     public void onInitialize() {
 
--- src/main/java/io/github/steviegt6/wheatdsmod/client/WheatDSModClient.java
+++ src/main/java/io/github/steviegt6/wheatdsmod/client/WheatDSModClient.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class WheatdsmodClient implements ClientModInitializer {
+public class WheatDSModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
