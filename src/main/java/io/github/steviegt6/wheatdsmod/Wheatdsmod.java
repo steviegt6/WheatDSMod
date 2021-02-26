f857f79e97366959b431cca6cd0b04b4fbf8b4c8
Stevie <xxlennygamerxx@gmail.com>
2021-02-25 22:12:32 -0800

    Initial mod files

---
+++ src/main/java/io/github/steviegt6/wheatdsmod/Wheatdsmod.java
@@ -0,0 +1,10 @@
+package io.github.steviegt6.wheatdsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Wheatdsmod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/steviegt6/wheatdsmod/client/WheatdsmodClient.java
@@ -0,0 +1,13 @@
+package io.github.steviegt6.wheatdsmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class WheatdsmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
