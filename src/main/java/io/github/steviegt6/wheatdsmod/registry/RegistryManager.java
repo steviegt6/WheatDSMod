32a82616f7fc2aa17b6a661bf59d7076218f84f1
Stevie <xxlennygamerxx@gmail.com>
2021-03-04 16:51:06 -0800

    Add our items to a villager's compostable-items catalogue

--- src/main/java/io/github/steviegt6/wheatdsmod/registry/RegistryManager.java
+++ src/main/java/io/github/steviegt6/wheatdsmod/registry/RegistryManager.java
@@ -1,6 +1,13 @@
 package io.github.steviegt6.wheatdsmod.registry;
 
 import io.github.steviegt6.wheatdsmod.logging.WheatLogger;
+import net.minecraft.entity.ai.brain.task.FarmerWorkTask;
+import net.minecraft.entity.ai.brain.task.GatherItemsVillagerTask;
+import net.minecraft.item.Item;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.List;
 
 /**
  * Helper class responsible for calling registry Load methods.
@@ -17,6 +24,26 @@ public class RegistryManager {
         BlockRegistry.registerCropBlocks();
         ItemRegistry.registerCropSeeds();
         RecipeRegistry.register();
+
+        try {
+            // https://stackoverflow.com/questions/3301635/change-private-static-final-field-using-java-reflection
+            // Jesus Christ, what a mess.
+            Field compostableField = FarmerWorkTask.class.getDeclaredField("COMPOSTABLES");
+            compostableField.setAccessible(true);
+
+            Field compostableModifiers = Field.class.getDeclaredField("modifiers");
+            compostableModifiers.setAccessible(true);
+            compostableModifiers.setInt(compostableField, compostableField.getModifiers() & ~Modifier.FINAL);
+
+            List<Item> compostables = (List<Item>) compostableField.get(null);
+            if (compostables != null) {
+                compostables.addAll(ItemRegistry.REGISTERED_SEEDS);
+                compostables.addAll(ItemRegistry.REGISTERED_CROPS);
+                compostableField.set(null, compostables);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
         WheatLogger.quickLogInfo("Registered content!");
     }
