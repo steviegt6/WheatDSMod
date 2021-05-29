d66e2b61ecef1fd1bf6412f225613bd82f15b0df
Stevie <xxlennygamerxx@gmail.com>
2021-05-28 22:14:15 -0700

    horse to-do notes

---
+++ src/main/java/io/github/steviegt6/wheatdsmod/mixin/HorseBaseEntityMixin.java
@@ -0,0 +1,10 @@
+package io.github.steviegt6.wheatdsmod.mixin;
+
+import net.minecraft.entity.passive.HorseBaseEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(HorseBaseEntity.class)
+public class HorseBaseEntityMixin {
+    // TODO: add custom value implementation for wheat
+    // TODO: add food acceptance
+}
